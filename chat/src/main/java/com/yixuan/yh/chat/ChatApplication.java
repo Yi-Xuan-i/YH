a903a5b3765517ed31a47a996a5d2a42eb96e45a
YXXHYH <2434462503@qq.com>
2025-07-18 15:37:32 +0800

    新微服务：新增 chat 微服务。

---
+++ chat/src/main/java/com/yixuan/yh/chat/ChatApplication.java
@@ -0,0 +1,12 @@
+package com.yixuan.yh.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+}
---
+++ chat/src/test/java/com/yixuan/study/microservices/user/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yixuan.study.microservices.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
