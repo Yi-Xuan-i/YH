c408449d89ac5c8f355272f640a8e8bdeaa7d346
YXXHYH <2434462503@qq.com>
2025-09-21 17:07:27 +0800

    新功能：处理评论增加消息，添加批量增加评论回复数。

--- video/video-core/src/main/java/com/yixuan/yh/video/consumer/VideoCommentConsumer.java
+++ video/video-core/src/main/java/com/yixuan/yh/video/consumer/VideoCommentConsumer.java
@@ -1,22 +1,24 @@
 package com.yixuan.yh.video.consumer;
 
 import com.yixuan.yh.video.mapper.VideoMapper;
+import com.yixuan.yh.video.mapper.VideoUserCommentMapper;
 import com.yixuan.yh.videoprocessor.mq.VideoCommentIncrMessage;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class VideoCommentConsumer {
 
     private final VideoMapper videoMapper;
+    private final VideoUserCommentMapper videoUserCommentMapper;
 
-    public VideoCommentConsumer(VideoMapper videoMapper) {
+    public VideoCommentConsumer(VideoMapper videoMapper, VideoUserCommentMapper videoUserCommentMapper) {
         this.videoMapper = videoMapper;
+        this.videoUserCommentMapper = videoUserCommentMapper;
     }
 
     @RabbitListener(
@@ -26,8 +28,14 @@ public class VideoCommentConsumer {
                     key = "video.comment.incr.queue"
             )
     )
-    public void handleVideoIncrCommentMessage(List<VideoCommentIncrMessage> videoCommentIncrMessages) {
-        videoMapper.updateCommentBatch(videoCommentIncrMessages);
+    @Transactional
+    public void handleVideoIncrCommentMessage(VideoCommentIncrMessage videoCommentIncrMessage) {
+        if (!videoCommentIncrMessage.getCommentIncrList().isEmpty()) { // 常理来说这里不会为null
+            videoMapper.updateCommentBatch(videoCommentIncrMessage.getCommentIncrList());
+        }
+        if (!videoCommentIncrMessage.getReplyIncrList().isEmpty()) {
+            videoUserCommentMapper.updateReplyBatch(videoCommentIncrMessage.getReplyIncrList());
+        }
     }
 
 }
