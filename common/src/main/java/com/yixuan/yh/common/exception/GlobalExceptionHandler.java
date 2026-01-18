8b45812ad44d05881a319fa2508a2f9f89caa344
YXXHYH <2434462503@qq.com>
2026-01-18 22:35:26 +0800

    规范全局异常处理器打印信息

--- common/src/main/java/com/yixuan/yh/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/com/yixuan/yh/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.yixuan.yh.common.exception;
 
 import com.yixuan.yh.common.response.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,27 +12,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(YHClientException.class)
     public Result<String> ex(YHClientException ex) {
-        ex.printStackTrace();
         return Result.error(ex.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(YHServerException.class)
     public Result<String> ex(YHServerException ex) {
-        ex.printStackTrace();
+        log.error("服务器异常", ex);
         return Result.error(ex.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Result<String> ex(MethodArgumentNotValidException ex) {
-        ex.printStackTrace();
         BindingResult bindingResult = ex.getBindingResult();
         // 解析异常信息
         List<String> errors = new ArrayList<>();
@@ -47,7 +47,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class) // 捕获所有异常
     public Result<String> ex(Exception ex) {
-        ex.printStackTrace();
+        log.error("服务器未知异常", ex);
         return Result.error(ex.getMessage());
     }
 
