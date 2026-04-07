384fcfe3b628844ca28e45c214104e7bf5218769
YXXHYH <2434462503@qq.com>
2026-04-07 12:03:03 +0800

    新增 xxl-job 配置类

---
+++ common/src/main/java/com/yixuan/yh/common/config/XxlJobConfig.java
@@ -0,0 +1,43 @@
+package com.yixuan.yh.common.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "xxl.job")
+@ConditionalOnProperty(
+        prefix = "xxl.job",
+        name = "admin.addresses"
+)
+public class XxlJobConfig {
+
+    private String accessToken;
+    private final Admin admin = new Admin();
+    private final Executor executor = new Executor();
+
+    @Data
+    public static class Admin {
+        private String addresses;
+    }
+
+    @Data
+    public static class Executor {
+        private String appname;
+        private int port;
+    }
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(admin.addresses);
+        xxlJobSpringExecutor.setAppname(executor.appname);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setPort(executor.port);
+        return xxlJobSpringExecutor;
+    }
+}
