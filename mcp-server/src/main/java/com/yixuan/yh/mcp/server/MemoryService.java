66987ff161cb5673dbeafd28af447c7abbd32615
YXXHYH <2434462503@qq.com>
2026-04-13 23:25:29 +0800

    初步完成实现长期记忆的工具

---
+++ mcp-server/src/main/java/com/yixuan/yh/mcp/server/MemoryService.java
@@ -0,0 +1,63 @@
+package com.yixuan.yh.mcp.server;
+
+import lombok.RequiredArgsConstructor;
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springaicommunity.mcp.annotation.McpToolParam;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class MemoryService {
+
+    private final VectorStore vectorStore;
+
+    @McpTool(name = "archival_memory_insert", description = "将重要的事实、知识或用户长期偏好存入归档记忆库（硬盘）")
+    public Mono<Boolean> archivalMemoryInsert(@McpToolParam(description = "需要存入的记忆") String memory) {
+        System.out.println("------------------");
+        System.out.println(memory);
+        System.out.println("------------------");
+        return Mono.fromCallable(() -> {
+            Document document = new Document(memory);
+            vectorStore.add(List.of(document));
+
+            return true;
+        }).subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @McpTool(name = "archival_memory_search", description = "当主内存中找不到相关信息时，搜索归档记忆库（硬盘）中的历史事实")
+    public Mono<String> archivalMemorySearch(@McpToolParam(description = "搜索词") String query) {
+        System.out.println("------------------");
+        System.out.println(query);
+        System.out.println("------------------");
+        return Mono.fromCallable(() -> {
+            SearchRequest request = SearchRequest.builder()
+                    .query(query)
+                    .topK(3)
+//                    .similarityThreshold(0.85)
+                    .build();
+
+            List<Document> docs = vectorStore.similaritySearch(request);
+
+            if (docs.isEmpty()) {
+                System.out.println("无");
+                return "未找到相关的历史记忆。";
+            }
+
+            String result = docs.stream()
+                    .map(Document::getText)
+                    .collect(Collectors.joining("\n---\n", "找到以下相关历史记录：\n", ""));
+            System.out.println(result);
+
+            return result;
+        }).subscribeOn(Schedulers.boundedElastic());
+    }
+
+}
