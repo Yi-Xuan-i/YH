d632e3c558aa995ac8066a7f6a612b5ba1b913d3
YXXHYH <2434462503@qq.com>
2026-05-26 17:16:02 +0800

    refactor(common): 移除 AWSUtils 中的 Spring 组件注解
    - 从 AWSUtils 类中移除了 @Component 注解
    - 调整了类的依赖注入方式以适应新的架构设计

--- common/src/main/java/com/yixuan/yh/common/utils/AWSUtils.java
+++ common/src/main/java/com/yixuan/yh/common/utils/AWSUtils.java
@@ -5,7 +5,6 @@ import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
